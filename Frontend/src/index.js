c7ae406922f7eb0ddab7a463bb15beae81db75c5
Siddhant-62 <siddhantkumar1997@gmail.com>
2025-05-11 14:54:15 +0530

    created nav bar

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from '../src/js/App';
+import Homepage from '../src/js/Homepage';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Homepage />
   </React.StrictMode>
 );
 
